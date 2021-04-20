8883e13895aafdb64718a4e6266c3a8b880229b7
nacholopezgarcia <nacholopezgarcia00@gmail.com>
2021-04-20 12:04:11 +0000

    Cambios de los modulos

---
+++ helpdesk_ignacio/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Helpdesk Ignacio",
+    "version": "14.0.1.0.0",
+    "author": "AEODOO, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/helpdesk_security.xml",
+        "security/ir.model.access.csv",
+        "views/helpdesk_menu.xml",
+        "views/helpdesk_view.xml"
+    ]
+} 
